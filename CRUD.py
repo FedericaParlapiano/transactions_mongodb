3727a16a342821886fc5188c13f80a70f4361a78
AriannaRonci <ariannaronci15@gmail.com>
2024-07-18 10:27:40 +0200

    modifiche crud

--- CRUD.py
+++ CRUD.py
@@ -3,34 +3,34 @@ import pprint
 from bson import ObjectId
 from pymongo import MongoClient
 
-connection_string = "mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.net/?appName=mongosh+2.2.10"
-
+connection_string = "mongodb+srv://arianna:arianna@cluster0.o61ssco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(connection_string)
 
-# Get reference to 'bank' database
+# get reference to database
 db = client.sample_analytics
 
-# Get reference to 'accounts' collection
+# get reference to collection
 accounts_collection = db.accounts
 customers_collection = db.customers
 transactions_collection = db.transactions
 
+# INSERT DOCUMENT
 new_account = {
     "account_id": 123456,
     "limit": 10000,
     "product": ["Brokerage", "Derivatives", "CurrencyService", "InvestmentFund", "InvestmentStock"]
 }
 
-# Write an expression that inserts the 'new_account' document into the 'accounts' collection.
+# inserts new_account document into the accounts collection
 result = accounts_collection.insert_one(new_account)
 
 document_id = result.inserted_id
 print(f"_id of inserted document: {document_id}")
 
 document_to_find = {"_id": ObjectId(document_id)}
-
 pprint.pprint(accounts_collection.find_one(document_to_find))
 
+# FIND DOCUMENT
 document_to_find = {"accounts": {"$in": [987709]}}
 
 cursor = customers_collection.find(document_to_find)
@@ -40,10 +40,10 @@ for document in cursor:
     num_docs += 1
     pprint.pprint(document)
     print()
-print("# of documents found: " + str(num_docs))
+print("Number of documents found: " + str(num_docs))
 
+# UPDATE DOCUMENT
 document_to_update = {"_id": ObjectId(document_id)}
-
 pprint.pprint(accounts_collection.find_one(document_to_update))
 
 add_product = {"$push": {"product": "Inv"}}
@@ -52,6 +52,7 @@ print("Documents updated: " + str(result.modified_count))
 
 pprint.pprint(accounts_collection.find_one(document_to_update))
 
+# DELETE DOCUMENT
 documents_to_delete = {"account_id": 123456}
 
 result = accounts_collection.delete_many(documents_to_delete)
