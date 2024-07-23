4ac991d520092c722527a570f48dfb569c028f74
AriannaRonci <ariannaronci15@gmail.com>
2024-07-23 16:10:38 +0200

    perdita di aggiornamento

--- perdita_aggiornamento_t1.py
+++ perdita_aggiornamento_t1.py
@@ -7,30 +7,36 @@ client1 = MongoClient('mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.
 
 session1 = client1.start_session()
 session1.start_transaction(
-    read_concern=ReadConcern("majority"),
+    read_concern=ReadConcern("local"),
     write_concern=WriteConcern("majority"),
 )
 
 
-db = client1['sample_analytics']
-myCollection = db['collection_prova']
+db = client1['negozio_abbigliamento']
+myCollection = db['capi_abbigliamento']
+id = 1
 
-doc = myCollection.find_one({'_id': '1'}, session=session1);
-initial_price = doc.get("price");
-print("T1 - Prezzo iniziale: ", initial_price);
+doc = myCollection.find_one({'_id': id}, session=session1);
+print("Documento da modificare: ", doc)
+initial_price = doc.get("prezzo");
+print("Prezzo iniziale: ", initial_price);
 
-time.sleep(5)
+time.sleep(3)
 
-myCollection.update_one({'_id': '1'}, {'$inc': {'price': 10}}, session=session1)
+try:
+    myCollection.update_one({'_id': id}, {'$inc': {'prezzo': -10}}, session=session1)
 
-modified_doc = myCollection.find_one({'_id': '1'}, session=session1);
-final_price = modified_doc.get("price");
-print("T1 - Prezzo finale: ", final_price);
-
-time.sleep(5)
+    modified_doc = myCollection.find_one({'_id': id}, session=session1);
+    final_price = modified_doc.get("prezzo");
+    print("Documento modificato: ", modified_doc)
+    print("Prezzo finale: ", final_price);
+except Exception as e:
+    print(f"Errore durante l'aggiornamento: {e}")
 
 try:
     session1.commit_transaction()
+    print("Transazione andata a buon fine.");
+
 except Exception as e:
     print(f"Errore durante il commit della transazione: {e}")
 
--- perdita_aggiornamento_t2.py
+++ perdita_aggiornamento_t2.py
@@ -6,26 +6,36 @@ client2 = MongoClient('mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.
 
 session2 = client2.start_session()
 session2.start_transaction(
-    read_concern=ReadConcern("majority"),
+    read_concern=ReadConcern("local"),
     write_concern=WriteConcern("majority"),
 )
 
-db = client2['sample_analytics']
-myCollection = db['collection_prova']
+db = client2['negozio_abbigliamento']
+myCollection = db['capi_abbigliamento']
+id = 1
 
-doc = myCollection.find_one({'_id': '1'}, session=session2);
-initial_price = doc.get("price");
-print("T2 - Prezzo iniziale: ", initial_price);
+doc = myCollection.find_one({'_id': id}, session=session2);
+print("Documento da modificare: ", doc)
+initial_price = doc.get("prezzo");
+print("Prezzo iniziale: ", initial_price);
 
-time.sleep(10)
+time.sleep(3)
 
 try:
-    myCollection.update_one({'_id': '1'}, {'$inc': {'price': 10}}, session=session2)
-    modified_doc = myCollection.find_one({'_id': '1'}, session=session2);
-    final_price = modified_doc.get("price");
-    print("T2 - Prezzo finale: ", final_price);
-    session2.commit_transaction()
+    myCollection.update_one({'_id': id}, {'$inc': {'prezzo': 10}}, session=session2)
+
+    modified_doc = myCollection.find_one({'_id': id}, session=session2);
+    final_price = modified_doc.get("prezzo");
+    print("Documento modificato: ", modified_doc)
+    print("Prezzo finale: ", final_price);
+
 except Exception as e:
     print(f"Errore durante l'aggiornamento: {e}")
+
+try:
+    session2.commit_transaction()
+    print("Transazione andata a buon fine.");
+except Exception as e:
+    print(f"Errore durante il commit della transazione: {e}")
 
 session2.end_session()
