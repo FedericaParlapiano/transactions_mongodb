64f6ab29a7cab576f10e91ed32822dece091c905
FedericaParlapiano <phede26@gmail.com>
2024-07-24 15:28:05 +0200

    write skew

--- write_skew_T1.py
+++ write_skew_T1.py
@@ -25,7 +25,7 @@ try:
 
     giacca = myCollection.find_one({'nome': 'Giacca'}, session=session1)
     prezzo_giacca = giacca.get("prezzo")
-    pantaloni = myCollection.find_one({'nome': 'Pantaloni corti'}, session=session1)
+    pantaloni = myCollection.find_one({'nome': 'Pantaloni'}, session=session1)
     prezzo_pantaloni = pantaloni.get("prezzo")
 
     prezzo_completo = prezzo_giacca + prezzo_pantaloni
@@ -42,21 +42,22 @@ try:
 
         giacca = myCollection.find_one({'nome': 'Giacca'}, session=session1)
         prezzo_giacca = giacca.get("prezzo")
-        pantaloni = myCollection.find_one({'nome': 'Pantaloni corti'}, session=session1)
+        pantaloni = myCollection.find_one({'nome': 'Pantaloni'}, session=session1)
         prezzo_pantaloni = pantaloni.get("prezzo")
         prezzo_completo = prezzo_giacca + prezzo_pantaloni
 
         abito = myCollection.find_one({'nome': 'Abito'}, session=session1)
         prezzo_abito = abito.get("prezzo")
 
-        print("T1 - Prezzo pantaloni dopo l'update: ", prezzo_pantaloni)
         print("T1 - Prezzo giacca dopo l'update: ", prezzo_giacca)
+        print("T1 - Prezzo pantaloni dopo l'update: ", prezzo_pantaloni)
         print("T1 - Prezzo completo giacca e pantaloni dopo l'update: ", round(float(prezzo_completo), 2))
         print("T1 - Prezzo abito dopo l'update: ", prezzo_abito)
-        time.sleep(5)
+        time.sleep(3)
         session1.commit_transaction()
         print("T1 - Commit")
         print("")
+        time.sleep(3)
     else:
         session1.abort_transaction()
         print("T1 - Abort: il prezzo del completo deve superare quello dell'abito di almeno 20 euro")
@@ -64,10 +65,9 @@ try:
 finally:
     session1.end_session()
 
-time.sleep(5)
 giacca = myCollection.find_one({'nome': 'Giacca'})
 prezzo_giacca = giacca.get("prezzo")
-pantaloni = myCollection.find_one({'nome': 'Pantaloni corti'})
+pantaloni = myCollection.find_one({'nome': 'Pantaloni'})
 prezzo_pantaloni = pantaloni.get("prezzo")
 prezzo_completo = prezzo_giacca + prezzo_pantaloni
 abito = myCollection.find_one({'nome': 'Abito'})
@@ -78,7 +78,7 @@ print("T1 - Prezzo pantaloni dopo il commit: ", round(float(prezzo_pantaloni), 2
 print("T1 - Prezzo abito dopo il commit: ", round(float(prezzo_abito), 2))
 print("T1 - Prezzo completo giacca e pantaloni dopo il commit: ", round(float(prezzo_completo), 2))
 
-if prezzo_completo >= prezzo_abito + 20:
+if prezzo_completo >= prezzo_abito:
     print("Il vincolo è rispettato")
 else:
     print("Attenzione! Il vincolo non è rispettato")
