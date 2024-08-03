e942c4d5d653d93bb34dc2340d92ee0b9926c890
FedericaParlapiano <phede26@gmail.com>
2024-08-03 12:14:20 +0200

    callback

--- aggiornamento_fantasma_T1.py
+++ aggiornamento_fantasma_T1.py
@@ -1,50 +1,67 @@
+from bson import Decimal128
 from pymongo import MongoClient, WriteConcern
+from pymongo.errors import PyMongoError
 from pymongo.read_concern import ReadConcern
 import time
 
-client = MongoClient('mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.net/?appName=mongosh+2.2.10')
+connection_string = "mongodb+srv://arianna:arianna@cluster0.o61ssco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+connection_string = "mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.net/?appName=mongosh+2.2.10"
+client = MongoClient(connection_string)
 
-session1 = client.start_session()
-session1.start_transaction(
-    read_concern=ReadConcern("snapshot"),
-    write_concern=WriteConcern("majority"),
-)
-
-try:
-
-    db = client['negozio_abbigliamento']
-    myCollection = db['capi_abbigliamento']
-
-    giacca = myCollection.find_one({'nome': 'Giacca'}, session=session1)
-    prezzo_giacca = giacca.get("prezzo")
-
-    print("T1 - Prezzo giacca (prima della modifica di T2): ", prezzo_giacca)
-    print("")
-
-    time.sleep(6)
-
-    abito = myCollection.find_one({'nome': 'Abito'}, session=session1)
-    prezzo_abito = abito.get("prezzo")
-
-    pantaloni = myCollection.find_one({'nome': 'Pantaloni'}, session=session1)
-    prezzo_pantaloni = pantaloni.get("prezzo")
-
-    prezzo_completo = prezzo_giacca + prezzo_pantaloni
-
-    print("T1 - Prezzo pantaloni (dopo le modifiche di T2): ", prezzo_pantaloni)
-    print("T1 - Prezzo completo giacca e pantaloni (dopo le modifiche di T2): ", round(float(prezzo_completo), 2))
-    print("T1 - Prezzo abito: ", prezzo_abito)
-    print("")
-
-    if prezzo_completo >= prezzo_abito:
-        print("T1 - Vincolo soddisfatto")
-    else:
-        session1.abort_transaction()
-        print("T1 - Vincolo non soddisfatto")
-
-finally:
-    session1.end_session()
+def callback(session):
+    capiCollection = session.client.negozio_abbigliamento.capi_abbigliamento.with_options(
+        write_concern=WriteConcern(w=1, j=False)
+    )
 
 
+    try:
 
+        giacca = capiCollection.find_one({'nome': 'Giacca'}, session=session)
+        prezzo_giacca = giacca.get("prezzo").to_decimal()
+
+        print("T1 - Prezzo giacca (prima della modifica di T2): ", prezzo_giacca)
+        print("\n\n\n")
+
+        time.sleep(7)
+
+        abito = capiCollection.find_one({'nome': 'Abito'}, session=session)
+        prezzo_abito = abito.get("prezzo").to_decimal()
+
+        pantaloni = capiCollection.find_one({'nome': 'Pantaloni'}, session=session)
+        prezzo_pantaloni = pantaloni.get("prezzo").to_decimal()
+
+        prezzo_completo = prezzo_giacca + prezzo_pantaloni
+
+        print("T1 - Prezzo pantaloni (dopo le modifiche di T2): ", prezzo_pantaloni)
+        print("T1 - Prezzo completo giacca e pantaloni (dopo le modifiche di T2): ", Decimal128(prezzo_completo))
+        print("T1 - Prezzo cappotto: ", prezzo_abito)
+        print("")
+
+        if prezzo_completo >= prezzo_abito:
+            print("T1 - Vincolo soddisfatto")
+        else:
+            session.abort_transaction()
+            print("T1 - Vincolo non soddisfatto")
+
+    finally:
+        session.end_session()
+
+
+def callback_wrapper(s):
+    callback(
+        session=s
+    )
+
+
+with client.start_session() as session:
+    try:
+        session.with_transaction(
+            callback_wrapper,
+            read_concern=ReadConcern(level="local"),
+            write_concern=WriteConcern(w=1, j=False)
+        )
+    except PyMongoError as e:
+        print(f"Transazione fallita: {e.args[0]}")
+
+client.close()
 
--- aggiornamento_fantasma_T2.py
+++ aggiornamento_fantasma_T2.py
@@ -1,64 +1,80 @@
+from bson import Decimal128
 from pymongo import MongoClient, WriteConcern
+from pymongo.errors import PyMongoError
 from pymongo.read_concern import ReadConcern
 import time
 
-# Connessione al client MongoDB
-client = MongoClient('mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.net/?appName=mongosh+2.2.10')
+connection_string = "mongodb+srv://arianna:arianna@cluster0.o61ssco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+connection_string = "mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.net/?appName=mongosh+2.2.10"
+client = MongoClient(connection_string)
 
-# Inizio della sessione con transazione
-session2 = client.start_session()
-session2.start_transaction(
-    read_concern=ReadConcern("snapshot"),
-    write_concern=WriteConcern("majority"),
-)
+def callback(session):
+    capiCollection = session.client.negozio_abbigliamento.capi_abbigliamento.with_options(
+        write_concern=WriteConcern(w=1, j=False)
+    )
 
-try:
-    # Selezione del database e della collezione
-    db = client['negozio_abbigliamento']
-    myCollection = db['capi_abbigliamento']
+    try:
 
-    abito = myCollection.find_one({'nome': 'Abito'}, session=session2)
-    prezzo_abito = abito.get("prezzo")
+        abito = capiCollection.find_one({'nome': 'Abito'}, session=session)
+        prezzo_abito = abito.get("prezzo").to_decimal()
 
-    giacca = myCollection.find_one({'nome': 'Giacca'}, session=session2)
-    prezzo_giacca = giacca.get("prezzo")
-    pantaloni = myCollection.find_one({'nome': 'Pantaloni'}, session=session2)
-    prezzo_pantaloni = pantaloni.get("prezzo")
+        giacca = capiCollection.find_one({'nome': 'Giacca'}, session=session)
+        prezzo_giacca = giacca.get("prezzo").to_decimal()
+        pantaloni = capiCollection.find_one({'nome': 'Pantaloni'}, session=session)
+        prezzo_pantaloni = pantaloni.get("prezzo").to_decimal()
 
-    prezzo_completo = prezzo_giacca + prezzo_pantaloni
+        prezzo_completo = prezzo_giacca + prezzo_pantaloni
 
-    print("T2 - Prezzo giacca prima dell'update: ", prezzo_giacca)
-    print("T2 - Prezzo pantaloni prima dell'update: ", prezzo_pantaloni)
-    print("T2 - Prezzo completo giacca e pantaloni: ", round(float(prezzo_completo), 2))
-    print("T2 - Prezzo abito: ", prezzo_abito)
-    print("")
-
-    # Verifica della condizione e aggiornamento del prezzo
-    if prezzo_completo >= prezzo_abito + 20:
-
-        time.sleep(4)
-        myCollection.update_one({'nome': 'Pantaloni'}, {'$set': {'prezzo': round(float(prezzo_pantaloni)-40, 2)}}, session=session2)
-
-        pantaloni = myCollection.find_one({'nome': 'Pantaloni'}, session=session2)
-        prezzo_pantaloni = pantaloni.get("prezzo")
-
-        print("T2 - Prezzo pantaloni dopo l'update: ", prezzo_pantaloni)
+        print("T2 - Prezzo giacca prima dell'update: ", prezzo_giacca)
+        print("T2 - Prezzo pantaloni prima dell'update: ", prezzo_pantaloni)
+        print("T2 - Prezzo completo giacca e pantaloni: ", round(float(prezzo_completo), 2))
+        print("T2 - Prezzo cappotto: ", prezzo_abito)
         print("")
 
-        myCollection.update_one({'nome': 'Giacca'}, {'$set': {'prezzo': round(float(prezzo_giacca) + 40, 2)}},
-                                session=session2)
+        # Verifica della condizione e aggiornamento del prezzo
+        if prezzo_completo >= prezzo_abito + 20:
 
-        giacca = myCollection.find_one({'nome': 'Giacca'}, session=session2)
-        prezzo_giacca = giacca.get("prezzo")
-        print("T2 - Prezzo giacca dopo l'update: ", prezzo_giacca)
-        print("")
+            time.sleep(4)
+            capiCollection.update_one({'nome': 'Pantaloni'}, {'$set': {'prezzo': Decimal128(prezzo_pantaloni-40)}}, session=session)
 
-        session2.commit_transaction()
-        print("T2 - Commit")
-        print("")
-    else:
-        session2.abort_transaction()
-        print("T2 - Abort: il prezzo del completo deve superare quello dell'abito")
-        print("")
-finally:
-    session2.end_session()
+            pantaloni = capiCollection.find_one({'nome': 'Pantaloni'}, session=session)
+            prezzo_pantaloni = pantaloni.get("prezzo").to_decimal()
+
+            print("T2 - Prezzo pantaloni dopo l'update: ", prezzo_pantaloni)
+            print("")
+
+            capiCollection.update_one({'nome': 'Giacca'}, {'$set': {'prezzo': Decimal128(prezzo_giacca+40)}},
+                                    session=session)
+
+            giacca = capiCollection.find_one({'nome': 'Giacca'}, session=session)
+            prezzo_giacca = giacca.get("prezzo").to_decimal()
+            print("T2 - Prezzo giacca dopo l'update: ", prezzo_giacca)
+            print("")
+
+            session.commit_transaction()
+            print("T2 - Commit")
+            print("")
+        else:
+            session.abort_transaction()
+            print("T2 - Abort: il prezzo del completo deve superare quello dell'cappotto")
+            print("")
+    finally:
+        session.end_session()
+
+def callback_wrapper(s):
+    callback(
+        session=s
+    )
+
+
+with client.start_session() as session:
+    try:
+        session.with_transaction(
+            callback_wrapper,
+            read_concern=ReadConcern(level="local"),
+            write_concern=WriteConcern(w=1, j=False)
+        )
+    except PyMongoError as e:
+        print(f"Transazione fallita: {e.args[0]}")
+
+client.close()
