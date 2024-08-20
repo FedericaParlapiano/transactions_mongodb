35fd0a450314e35ac44aea392631e59702ff7fa3
FedericaParlapiano <75133721+FedericaParlapiano@users.noreply.github.com>
2024-08-20 09:43:24 +0200

    Update lettura_sporca_t2.py

--- lettura_sporca_t2.py
+++ lettura_sporca_t2.py
@@ -2,8 +2,7 @@ from pymongo import MongoClient, WriteConcern
 from pymongo.errors import PyMongoError
 from pymongo.read_concern import ReadConcern
 
-connection_string = "mongodb+srv://arianna:arianna@cluster0.o61ssco.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-connection_string = "mongodb+srv://federica:federica@cluster1.1mnlttb.mongodb.net/?appName=mongosh+2.2.10"
+connection_string = "..."
 client2 = MongoClient(connection_string)
 
 
